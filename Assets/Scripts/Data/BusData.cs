cb852324ad3a2c8f0caf72fcd350e7b40cbd366a
Taylan Özdoğan <ozdogantaylan@gmail.com>
2025-06-02 03:06:07 +0300

    Bus capacity fixed.

--- Assets/Scripts/Data/BusData.cs
+++ Assets/Scripts/Data/BusData.cs
@@ -5,13 +5,12 @@ using System;
 public class BusData
 {
     public PassengerColor color;
-    public int capacity = 3;
+    public readonly int capacity = 3;
     // public string busId; // Optional: for specific bus types or tracking
     // public Vector3[] pathPoints; // Optional: for predefined bus paths
 
-    public BusData(PassengerColor color, int capacity)
+    public BusData(PassengerColor color)
     {
         this.color = color;
-        this.capacity = capacity;
     }
 }
